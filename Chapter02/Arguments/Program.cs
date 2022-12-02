60a880ad53a399c08c57c595a4c661a536cce762
Philipp Buchinger <buchinger@proton.me>
2022-12-02 17:10:46 +0100

    added 'Arguments' in Chapter2

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,27 @@
+﻿if (args.Length < 3)
+{
+    WriteLine("You must specify two colors and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return; // stop running
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0],
+    ignoreCase: true
+);
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1],
+    ignoreCase: true
+);
+
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support changing of cursor size.");
+}
--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -1,17 +1,8 @@
-﻿// numbered positional arguments
+﻿Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
 
-int numberOfApples = 12;
-decimal pricePerApple = 0.35M;
-
-Console.WriteLine(
-    format: "{0} apples cost {1:C}",
-    arg0: numberOfApples,
-    arg1: pricePerApple
-);
-
-// five or more paramater values cannot use named arguments
-
-Console.WriteLine(
-    format: "{0} {1} lived in {2}, {3} and worked in the {4}, team at {5}.",
-    "Roger", "Cevung", "Stockholm", "Sweden", "Education", "Optimizely"
+WriteLine("\nKey: {0}, Char: {1}, Modifiers: {2}",
+    arg0: key.Key,
+    arg1: key.KeyChar,
+    arg2: key.Modifiers
 );
