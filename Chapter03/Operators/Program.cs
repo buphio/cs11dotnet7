aad82b9e6371e19d764cc25e3c2079c6605190ae
Philipp Buchinger <buchinger@proton.me>
2023-01-06 08:01:29 +0100

    added Chapter03

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,12 @@
+﻿var a = 3;
+var b = a++;
+WriteLine($"a is {a}, b is {b}");
+
+var e = 11;
+var f = 3;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f is {e + f}");
+WriteLine($"e - f is {e - f}");
+WriteLine($"e * f is {e * f}");
+WriteLine($"e / f is {e / f}");
+WriteLine($"e % f is {e % f}");
